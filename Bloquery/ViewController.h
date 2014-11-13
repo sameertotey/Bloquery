e329b7c133eb71260cde5b8f1e60ae6f1f4bd723
Sameer Totey <sameertotey@Sameers-MacBook-Pro.local>
2014-11-13 10:01:48 -0700

    Initial Commit

---
+++ Bloquery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bloquery
+//
+//  Created by Sameer Totey on 11/13/14.
+//  Copyright (c) 2014 Sameer Totey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BloqueryTests/BloqueryTests.m
@@ -0,0 +1,40 @@
+//
+//  BloqueryTests.m
+//  BloqueryTests
+//
+//  Created by Sameer Totey on 11/13/14.
+//  Copyright (c) 2014 Sameer Totey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BloqueryTests : XCTestCase
+
+@end
+
+@implementation BloqueryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
